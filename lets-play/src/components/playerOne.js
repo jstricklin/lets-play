8dac1ae485a5a49232b91bf9b8297df8bf50d903
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-10-12 11:44:42 -0600

    created p1 and p2

--- lets-play/src/components/playerOne.js
+++ lets-play/src/components/playerOne.js
@@ -1,1 +1,23 @@
+import React from ('react')
 import './card'
+
+class PlayerOne extends Component {
+    render() {
+        return (
+          <div>
+            <BootstrapTable data={this.props.data}>
+              <TableHeaderColumn isKey dataField='id'>
+                ID
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField='name'>
+                Name
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField='value'>
+                Value
+              </TableHeaderColumn>
+            </BootstrapTable>
+          </div>
+        );
+    }
+}
+export default PlayerOne
